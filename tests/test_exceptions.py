18a1904c7f264b98a6ee5bd17332eaaada058b45
Joe <jgliniecki@gmail.com>
2022-11-02 12:47:42 -0500

    Update exception example

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -4,10 +4,10 @@ def test_exception_basics():
     try:
         x = 1/0
         pass      # This line will not be reached if previous line throws an exception
-    except ZeroDivisionError:  # Except clause can list more than one exception: except(ZeroDivisionError, RuntimeError)
+    except ZeroDivisionError as e:  # Except clause can list more than one exception: except(ZeroDivisionError, RuntimeError)
         # If no exception in try block, this block is skipped
         # If try block throws something other than a ZeroDivisionError, then it's an unhandled exeption and execution stops
-        print('You tried to divide by zero!')
+        print('You tried to divide by zero: ', e)
     else:
         print('Optional else, No exception raised if we got here, useful to isolate code that can generate exception to try block and put the rest here')
     finally:
