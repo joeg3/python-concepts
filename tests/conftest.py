c78d5e507866650f03bee7695b9b7d33eebcfcd7
Joe <jgliniecki@gmail.com>
2023-01-04 08:01:25 -0600

    File i/o and exceptions examples

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,9 +18,19 @@ def config(request):
 
 @pytest.fixture(scope='function')
 def open_close_file_path(request):
-    file_name = request.param
-    print('^^^^^^^^^^^^^ About to open file')
+    file_name = request.param # We get file name from tests that use this fixture
     file = open(file_name)
     yield file
-    print('^^^^^^^^^^^^^ About to close file')
     file.close()
+
+@pytest.fixture(scope='function')
+def read_file_name():
+    return 'text_files/read_from_file.txt'
+
+@pytest.fixture(scope='function')
+def write_file_name():
+    return 'text_files/write_to_file.txt'
+
+@pytest.fixture(scope='function')
+def json_file_name():
+    return 'text_files/json_file.json'
--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,17 +1,22 @@
 
 
 def test_exception_basics():
+    # For Python, this is called a try-except block
+    # If exception thrown, you get what's called a traceback
+    # When experimenting with code and you get a traceback, look at last line to see the
+    # type of exception that was raised so you know what to use in the except block you'll write.
     try:
-        x = 1/0
+        quotient = 1/0
         pass      # This line will not be reached if previous line throws an exception
     except ZeroDivisionError as e:  # Except clause can list more than one exception: except(ZeroDivisionError, RuntimeError)
         # If no exception in try block, this block is skipped
         # If try block throws something other than a ZeroDivisionError, then it's an unhandled exeption and execution stops
         print('You tried to divide by zero: ', e)
     else:
-        print('Optional else, No exception raised if we got here, useful to isolate code that can generate exception to try block and put the rest here')
+        # Optional else, No exception raised if we got here, useful to isolate code that can generate exception to try block and put the rest here
+        print(quotient)
     finally:
-        print('The optional finally clause runs whether or not the try statement produces an exception.')
+        pass # The optional finally clause runs whether or not the try statement produces an exception
 
 def test_raise_exception():
     try:
